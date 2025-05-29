244b90384bac2e04e027a68696054f48b88c1855
Arun Kumar <111792111+arunk-dev-hub@users.noreply.github.com>
2025-05-29 21:08:37 +0530

    Create xyz.java

---
+++ xyz.java
@@ -0,0 +1,11 @@
+class Job {
+    int jobId;
+    int profit;
+    int deadline;
+
+    public Job(int jobId, int profit, int deadline) {
+      this.jobId = jobId;
+      this.profit = profit;
+      this.deadline = deadline;
+    }
+  }
